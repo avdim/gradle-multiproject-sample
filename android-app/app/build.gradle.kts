cb8b720c30fe7bce7aad743c618ff32fdff48cdc
avdim <avdim@mail.ru>
2021-02-04 18:46:41 +0300

    version2

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("com.example.android-application")
+    id("ru.example.version")
 }
 
 group = "${group}.android-app"
 
 dependencies {
     implementation("com.example.myproduct.user-feature:table2")
+    Versions.kotlinVersion
 }
